ec2c9ca3b7b08842cd99a13c582ffa8f66ba7ab2
Ashley <ashley@Jamess-MacBook-Air.local>
2018-10-11 06:04:05 -0500

    commit 7, edit and new added

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Pokemon = require('../models/pokemon')
 //Stuffs
 router.get('/', (req, res) => {
-	res.render('index.ejs', {pokemon: Pokemon});
+	res.render('index.ejs', {pokemon: Pokemon})
 });
 
 router.get('/:id', (req, res) => {
@@ -14,12 +14,35 @@ router.get('/:id', (req, res) => {
 	});
 });
 
+router.post('/', (req, res) => {
+	Pokemon.push(req.body)
+	res.redirect('/pokemon')
+});
 
 
+router.get('/new', (req, res) => {
+	res.render('new.ejs')
+});
+
+//Delete
+router.delete('/:id', (req, res) => {
+	Pokemon.splice(req.params.id, 1),
+	res.direct('/pokemon')
+});
+
+//edit route established
+router.get('/:id/edit', (req, res) => {
+	res.render('edit.ejs', {
+		pokemon: Pokemon[req.params.id]
+	});
+});
 
 
-
-
+//edit route
+router.put('/:id', (req, res) => {
+	Pokemon[req.params.id] = req.body;
+	res.redirect('/pokemon' + [req.params.id])
+})
 
 
 
